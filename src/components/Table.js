3db9fc78c511e4aea6adc4a58217c421d7e62378
Valdemar Børresen <valdemab@stud.ntnu.no>
2022-10-17 12:04:15 +0200

    minor changes

--- src/components/Table.js
+++ src/components/Table.js
@@ -10,31 +10,12 @@ const url = "https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=US
 
 
 const Table = () => {
-//   const [bitcoindata, setBitcoindata] = useState([])
-//   const [datastart, setDatastart] = useState(0)
-//   const [dataend, setDataend] = useState(20)
   const [activepage, setActivepage] = useState(1)
-//   const [slicedlist, setSlicedlist] = useState([])
-//   const [pages, setPages] = useState([1])
   let pagelen = 20  
 
   const {isLoading, data, refetch, isError, error} = useBitcoinData(activepage, pagelen)
-  //   setBitcoindata(data?.data.Data.Data.reverse())
+
   let pages = [1,2,3,4,5]
-
-//   const getData = async() => {
-//     const info = await axios.get(url)
-//     setBitcoindata(info.data.Data.Data.reverse())
-//     // console.log(bitcoindata)
-//     setSlicedlist(bitcoindata.slice(datastart, dataend))
-//     let pagenum = Math.ceil(bitcoindata.length/20)
-//     setPages(Array(pagenum).fill(1,1,pagenum).map((x,i)=>i))
-//   }
-  
-//   useEffect(() => {
-//     getData();
-//   },[])
-
 
 
   function dateConverter(datadate) {
@@ -42,16 +23,8 @@ const Table = () => {
   };
 
   const handlePageChange = (event) => {
-    // setDatastart((event-1)*pagelen)
     setActivepage(event)
-    // if (event*pagelen > bitcoindata.length) {
-    //   setDataend(bitcoindata.length)
-    // } else {
-    //   setDataend(event*pagelen)
-    // }
-    // setSlicedlist(bitcoindata.slice(datastart, dataend))
-    console.log(event)
-    // console.log(bitcoindata)
+    // console.log(event)
   }
 
   if (isLoading) {
@@ -87,7 +60,7 @@ const Table = () => {
       </div>
       <div className='PaginationContainer'>
         {pages.map((pagenumber, key) => {return(
-          <button onClick={() => handlePageChange(pagenumber)} className={`PaginationButton ${key === activepage ? "active" : ""}`}>{pagenumber}</button>
+          <button onClick={() => handlePageChange(pagenumber)} className={`PaginationButton ${key == activepage ? "active" : ""}`}>{pagenumber}</button>
         )})}
       </div>
     </>
